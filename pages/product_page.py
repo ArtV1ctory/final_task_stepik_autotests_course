0e96390331c7b1bddd1bc8dfb36edd06782a4d5e
ArtV1ctory <vikaartemeva98@yandex.ru>
2019-09-22 16:54:23 +0300

    The functions of checking the login link have been moved to be able to go to it from other pages

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,6 +1,6 @@
 from .base_page import BasePage
 from .locators import ProductPageLocators
-from selenium.common.exceptions import NoAlertPresentException  # в начале файла
+from selenium.common.exceptions import NoAlertPresentException
 import math
 
 
